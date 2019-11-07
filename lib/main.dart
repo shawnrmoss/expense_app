d5ef9e8e919d7fcf14c32cefb53ea3f5ea7f8f79
shawn <shawnrmoss@gmail.com>
2019-11-06 21:39:24 -0600

    Lesson 119

--- lib/main.dart
+++ lib/main.dart
@@ -127,13 +127,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: (MediaQuery.of(context).size.height -
                         appBar.preferredSize.height -
                         MediaQuery.of(context).padding.top) *
-                    0.4),
+                    0.3),
             Container(
               child: TransactionList(_userTransactions, _deleteTransaction),
               height: (MediaQuery.of(context).size.height -
                       appBar.preferredSize.height -
                       MediaQuery.of(context).padding.top) *
-                  0.6,
+                  0.7,
             ),
           ],
         ),
